46c1afe0a70e6aef6787a1973dbb5e065d9cb4a8
Pincirolli <efpincirolli@indracompany.com>
2024-09-23 17:10:52 -0300

    primera subida

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output
 from dash import dash_table, State
 from flask import Flask
